ecd0af322f8d8e9ee80f09cbecc8398d0a8bad94
lqsa <1658616397@qq.com>
2018-11-29 16:11:29 +0800

    今天又是繁忙的一天

--- app/lq_liblary/phpEmail.php
+++ app/lq_liblary/phpEmail.php
@@ -10,22 +10,41 @@ require_once '../../PHPMailer-master/src/PHPMailer.php';
 require_once '../../PHPMailer-master/src/SMTP.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-class phpEmail{
+class phpEmail
+{
     public function __construct()
     {
-        $this->email=new PHPMailer();
-    }
-    public function sendEmail(){
+        $this->email = new PHPMailer();
         //作用未知 文档上的
-        $this->email->SMTPDebug=2;
+        $this->email->SMTPDebug = 2;
         //使用SFtp服务器
         $this->email->isSMTP();
-        $this->email->Host='';
-        $this->email->SMTPAuth=true;
-        $this->email->Username='';
-        $this->email->Password='';
-        $this->email->SMTPSecure='tls';
-        $this->email->Port=587;
+        $this->email->Host = '';
+        $this->email->SMTPAuth = true;
+        $this->email->Username = '';
+        $this->email->Password = '';
+        $this->email->SMTPSecure = 'tls';
+        $this->email->Port = 587;
+    }
 
+    public function sendEmail()
+    {
+        //Recipients
+        $this->email->setFrom('from@example.com', 'Mailer');
+        // Add a recipient
+        $this->email->addAddress('ellen@example.com');               // Name is optional
+        $this->email->addReplyTo('info@example.com', 'Information');
+        $this->email->addCC('cc@example.com');
+        $this->email->addBCC('bcc@example.com');
+        //Attachments
+        $this->email->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        $this->email->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        //Content
+        $this->email->isHTML(true);                                  // Set email format to HTML
+        $this->email->Subject = 'Here is the subject';
+        $this->email->Body = 'This is the HTML message body <b>in bold!</b>';
+        $this->email->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $this->email->send();
+        echo 'Message has been sent';
     }
 }
